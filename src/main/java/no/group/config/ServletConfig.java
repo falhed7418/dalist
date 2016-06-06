3386228ccda19bab5ab9183d93ff4b69e0272c01
Roberto <roberto.bernabe@gmail.com>
2016-06-05 22:03:48 -0500

    adding pebble

--- src/main/java/no/group/config/ServletConfig.java
+++ src/main/java/no/group/config/ServletConfig.java
@@ -1,5 +1,54 @@
 package no.group.config;
 
-public class ServletConfig {
-    
+import javax.servlet.ServletContext;
+import com.mitchellbosecke.pebble.PebbleEngine;
+import com.mitchellbosecke.pebble.loader.Loader;
+import com.mitchellbosecke.pebble.loader.ServletLoader;
+import com.mitchellbosecke.pebble.spring4.PebbleViewResolver;
+import com.mitchellbosecke.pebble.spring4.extension.SpringExtension;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class ServletConfig extends WebMvcConfigurerAdapter {
+
+    private ServletContext servletContext;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/css/**").addResourceLocations("/css/");
+        registry.addResourceHandler("/js/**").addResourceLocations("/js/");
+    }
+
+    @Bean
+    public Loader templateLoader() {
+        return new ServletLoader(servletContext);
+    }
+
+    @Bean
+    public SpringExtension springExtension() {
+        return new SpringExtension();
+    }
+
+    @Bean
+    public PebbleEngine pebbleEngine() {
+        return new PebbleEngine.Builder()
+            .loader(templateLoader())
+            .extension(springExtension())
+            .build();
+    }
+
+    @Bean
+    public PebbleViewResolver viewResolver() {
+        PebbleViewResolver viewResolver = new PebbleViewResolver();
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".html");
+        viewResolver.setPebbleEngine(pebbleEngine());
+        return viewResolver;
+    }
+
 }
